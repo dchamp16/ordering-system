92f67a240b05f446a51c01897026a484d9e779e8
dchamp16 <dchamp16@gmail.com>
2024-12-17 14:26:04 -0700

    udpated

--- jwt-example/app.ts
+++ jwt-example/app.ts
@@ -1,3 +1,47 @@
 import connectDB from "./src/config/db";
+import mongoose, { Document, Model } from "mongoose";
 
-connectDB();
+function testBluePrint<T>(data: T): T {
+  return data;
+}
+
+interface Animal {
+  name: string;
+  gender: string;
+}
+
+interface Dog extends Animal {
+  kindOfDog: string;
+}
+
+const test = testBluePrint<Dog>({
+  name: "hello",
+  gender: "Male",
+  kindOfDog: "nice dog",
+});
+
+console.log(test);
+
+function animalInfo(info: Dog) {
+  console.log(`
+    Name: ${info.name}
+    Gender: ${info.gender}
+    kindOfDog: ${info.kindOfDog}
+    `);
+}
+
+animalInfo({
+  name: "Pute",
+  gender: "Male",
+  kindOfDog: "Shiba-Inu",
+});
+
+const testPet: Dog = {
+  name: "Test Pet",
+  gender: "Male",
+  kindOfDog: "doggo",
+};
+
+console.log(testPet);
+
+// connectDB();
