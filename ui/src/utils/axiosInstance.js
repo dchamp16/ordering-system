99668b628b9ee1c5057042938c426b4d55cd03f0
dchamp16 <dchamp16@gmail.com>
2025-02-13 21:15:02 -0700

    Update axiosInstance.js

--- ui/src/utils/axiosInstance.js
+++ ui/src/utils/axiosInstance.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const PORT = 3000;
+
+console.log(PORT)
+
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: `http://localhost:${PORT}/api`,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
