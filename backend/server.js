78e80f7e7e77e22d51f5c4ac66dfb548526c825e
Peter Ramos <86265240+dchamp16@users.noreply.github.com>
2025-04-03 16:32:24 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,10 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 // Allow both local dev and deployed frontend
-const allowedOrigin = process.env.FRONTEND_URL || "http://localhost:5173";
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://ordering-system-heto.vercel.app",
+];
 
 
 // Create HTTP Server
@@ -42,7 +45,7 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
