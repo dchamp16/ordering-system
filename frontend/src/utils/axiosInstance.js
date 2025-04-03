cf5236777265afdc9a0aa541353e178c81394d79
Peter Ramos <86265240+dchamp16@users.noreply.github.com>
2025-04-03 15:47:15 -0600

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
+const baseURL = import.meta.env.VITE_API_BASE_URL ;
+
+console.log('Base URL:', baseURL);
 
 const axiosInstance = axios.create({
     baseURL: `${baseURL}/api`,
